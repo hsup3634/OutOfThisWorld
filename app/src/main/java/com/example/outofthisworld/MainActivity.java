5499e6d51f8497c01c008ac024f6a8e6e374848a
Sarah Memon <z5160295@ad.unsw.edu.au>
2019-11-03 19:05:04 +1100

    Bg is kinda squished - will fix later. Added landing page (UI not complete), but you can switch pages now

--- app/src/main/java/com/example/outofthisworld/MainActivity.java
+++ app/src/main/java/com/example/outofthisworld/MainActivity.java
@@ -1,15 +1,33 @@
 package com.example.outofthisworld;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ConstraintLayout home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void homeClick (View v) {
+        View view = (View) findViewById(R.id.home);
+        Context context = view.getContext();
+        Intent intent = new Intent(context, HomeLanding.class);
+        context.startActivity(intent);
+
+
+
 
     }
 }
